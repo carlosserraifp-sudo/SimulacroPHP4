bdab8e07c3df64f79c2a97e79bab267c0009da8b
Carlos <carlos.serra.ifp@gmail.com>
2026-05-28 08:27:03 +0200

    arreglo

--- VIEW/eventos.php
+++ VIEW/eventos.php
@@ -195,7 +195,7 @@ $eventos = $eventController->index();
             <div class="eventos-disponibles__grid">
                 <?php
                 // Display free events from the database
-                $freeEvents = $eventController->index();
+                $freeEvents = $eventController->indexfree();
 
                 if (!empty($freeEvents)) {
                     $count = 0;
